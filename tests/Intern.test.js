830b00f6f9a03fa16e43b72e162362a27cab95fd
TS93 <tschram93@yahoo.com>
2021-10-13 22:19:35 -0400

    intern test finished with manager test

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,1 +1,21 @@
 const Intern = require('../library/Intern')
+
+
+test('create intern', () => {
+    const intern = new Intern('Travis', 28, 'TSchram93@yaho.com', 'Michigan State University');
+
+    expect(intern.school).toEqual(expect.any(String));
+});
+
+
+test('school', () => {
+    const intern = new Intern('Travis', 28, 'TSchram93@yaho.com', 'Michigan State University');
+
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+});
+
+test('intern role', () => {
+    const intern = new Intern('Travis', 28, 'TSchram93@yaho.com');
+
+    expect(intern.jobSelection()).toEqual("Intern");
+});
--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,1 +1,15 @@
 const Manager = require('../library/Manager')
+
+
+test('create manager', () => {
+    const manager = new Manager('Travis', 28, 'Tschram93@gmail.com', 1);
+
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+
+test('manager role', () => {
+    const manager = new Manager('Travis', 28, 'Tschram93@gmail.com');
+
+    expect(manager.jobSelection()).toEqual("Manager");
+});
