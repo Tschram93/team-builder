d1bc208e0fa77ed565253e38e86f5c6c859a979b
TS93 <tschram93@yahoo.com>
2021-10-15 18:08:07 -0400

    Began psuedo for script to generate html file of the staff

--- src/generate..js
+++ src/generate..js
@@ -1,22 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
+// REMINDER: Use bootstrap classes 
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Dream Team</title>
-</head>
+//  Create cards for each of the following: Manager, Engineer, Intern
 
-<body>
+// TODO: Manager Card
+
+// TODO: Engineer Card
+
+// TODO: Intern
 
 
+// Function for each job position
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
-</body>
+// TODO: MANAGER
 
-</html>
+// TODO: ENGINEER
+
+// TODO: INTERN
+
+
+
+// TODO: Generate the page as HTML file
+
+
+// TODO: export module
