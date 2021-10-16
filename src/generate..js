1a2978ae60d805d7bedbf65d3d5bfab2c8c1ace4
TS93 <tschram93@yahoo.com>
2021-10-15 20:27:47 -0400

    manager card basic template set up(not finished

--- src/generate..js
+++ src/generate..js
@@ -2,10 +2,36 @@
 
 // Create cards for each of the following: Manager, Engineer, Intern
 // Help video for html javascript templates https://www.youtube.com/watch?v=NgF9-pdTDGs
+// TODO: Templates each need a function
 
 // TODO: Manager Card
 
 const createManagerCard = ;
+
+
+// Template for Manager html
+// Needs nested template literals 
+
+// `    <div class="d-flex justify-content-center flex-wrap">
+// <div class="card bg-secondary bg-opacity-25" style="width: 18rem;">
+//     <div class="card-body">
+//         <!-- Get the blue background top to take up entirety with no grey/whitespace -->
+//         <section class="bg-primary text-white pb-1">
+//             <h5 class="card-title">Jared </h5>
+//             <h6 class="card-subtitle mb-2">Manager</h6>
+//         </section>
+//         <!-- Figure out how to remove the black solid circles (bullets) for the list -->
+//         <div>
+//             <ul class="list-unstyled lh-lg mt-4 d-flex flex-wrap justify-content-center">
+//                 <li class="bg-light border border-secondary mb-1">ID: 1</li>
+//                 <li class="bg-light border border-secondary mb-1">Email: <a
+//                         href="mailto: jaredm@fakemail.com">jared@fakemail.com</a></li>
+//                 <li class="bg-light border border-secondary">Office Number: <span class="officeNum">36C</span>
+//                 </li>
+//             </ul>
+//         </div>
+//     </div>    `
+
 
 // TODO: Engineer Card
 
