5f4d9e18e033c3bc65cfd679df04cb50561ed56f
TS93 <tschram93@yahoo.com>
2021-10-15 23:06:07 -0400

    html template

--- src/generate..js
+++ src/generate..js
@@ -106,15 +106,46 @@ employeeContent = (data) => {
             emptyArray.push(internRole)
         }
     }
-    let cardInfo = emptyArray.join('');
+    const cardInfo = emptyArray.join('');
     const compileEmployees = (templateInjection);
 }
 
 
 // TODO: Generate the page as HTML file
 // Need to insert/inject html template into a file
-const templateInjection =
+const templateInjection = (cardInfo) => {
+    `
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Team</title>
+</head>
+<header class="bg-danger text-white py-4">
+    <h1 class="text-center">My Team</h1>
+</header>
+
+<body>
+    <div class="container">
 
 
-    // export module
-    module.exports = generate;
+    </div>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+</script>
+
+</html>
+`;
+}
+
+// export module
+module.exports = generate;
